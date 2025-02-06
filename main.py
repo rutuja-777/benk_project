89d562656b2701b3ad74683278a5554dbc4f03c6
rutuja <rutujamathakari@gmail.com>
2025-02-06 14:32:44 +0530

    added print_message feature

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+def print_message(message):
+    print(f'hi, I have recieved {message} !')
+    return
+
 if __name__== 'main':
-    print('Hello Git !')
+    print_message('Hello, lets learn about git !')
