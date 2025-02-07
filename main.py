219777cdb71dffe70b9624adbfc62a63a7890039
rutuja <rutujamathakari@gmail.com>
2025-02-07 11:33:39 +0530

    adding add function

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ def print_message(message):
     print(f'hi, I have recieved {message} !')
     return
 
+def add(a,b):
+    return a+b
+
 if __name__== 'main':
     print_message('Hello, lets learn about git !')
