cf894be473ab075045ed797a5f2c2f21b6e43c0e
rutuja <rutujamathakari@gmail.com>
2025-02-07 12:56:52 +0530

    adding new functions and git reset help

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ def print_message(message):
     print(f'hi, I have recieved {message} !')
     return
 
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+    
+
 if __name__== 'main':
     print_message('Hello, lets learn about git !')
