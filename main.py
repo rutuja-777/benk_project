a7f0b5d45d20431b6dfd33d92ad7950511b61107
rutuja <rutujamathakari@gmail.com>
2025-02-07 11:51:49 +0530

    adding mul feature

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ def print_message(message):
 def add(a,b):
     return a+b
 
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+    
+
 if __name__== 'main':
     print_message('Hello, lets learn about git !')
